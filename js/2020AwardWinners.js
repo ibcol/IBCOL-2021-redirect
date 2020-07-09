22bb740806d92fac215c053d0c5ec90cbeceac91
RomanYuen <jasonyuen1001@gmail.com>
2020-07-09 12:09:53 +0800

    section of "Participating Blockchain Olympiads" and "IBCOL Award Winners" updated

---
+++ js/2020AwardWinners.js
@@ -0,0 +1,12 @@
+var awardWinnersArr = [
+    './2020EXPO/2020-IBCOL-Finalists/HK-12-representatives/BCrew/index.html',
+    './2020EXPO/2020-IBCOL-Finalists/BD-12-representatives/Team%20Digital%20Innovation/index.html',
+    './2020EXPO/2020-IBCOL-Finalists/NL-12-representatives/Neuro Trace/index.html',
+    './2020EXPO/2020-IBCOL-Finalists/HK-12-representatives/Microcent/index.html',
+    './2020EXPO/2020-IBCOL-Finalists/BD-12-representatives/DU_Nimbus/index.html',
+    './2020EXPO/2020-IBCOL-Finalists/HK-12-representatives/BCrew/index.html'
+];
+
+function awardWinners(index) {
+    window.open(awardWinnersArr[index]);
+}
