9ee92c5d517d11b6252f0c59753139b310a7764c
RomanYuen <jasonyuen1001@gmail.com>
2020-07-09 16:12:37 +0800

    display: none "IBCOL 2020 Prizes Winners" and "IBCOL 2020 Award of Merit"

--- js/2020AwardWinners.js
+++ js/2020AwardWinners.js
@@ -10,3 +10,201 @@ var awardWinnersArr = [
 function awardWinners(index) {
     window.open(awardWinnersArr[index]);
 }
+
+// award
+
+var indexArr = [
+    ['01','02'],
+    ['03','04'],
+    ['05','06'],
+    ['07','08'],
+    ['09','10'],
+    ['11','12'],
+    ['01','02'],
+    ['03','04'],
+    ['05','06'],
+    ['07','08'],
+    ['09','10'],
+    ['11','12'],
+    ['01','02'],
+    ['03','04'],
+    ['05','06'],
+    ['07','08'],
+    ['09','10'],
+    ['11','12']
+]
+
+var teamNameAndID = [
+    ['Hyperactive Oranges (BD-01)', 'Team Leads Chain (BD-02)'],
+    ['DU_Nimbus (BD-03)', 'Team Digital Innovation (BD-04)'],
+    ['TORR (BD-05)', 'WEIB_3:1  (BD-06)'],
+    ['Brogrammers (BD-07)', 'Aviato (BD-08)'],
+    ['DU_Hyperledger (BD-09)', 'Cosmic Crew (BD-10)'],
+    ['Code Nine BD (BD-11)', 'BUET_SKAMmers_1 (BD-12)'],
+    ['Blossom Block (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom Block (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)'],
+    ['Blossom (CA-01)', 'JustBOT (CA-05)']
+];
+
+var pjName = [
+    ['Health Ledger', 'Track, Trace and Verify Insulin Medicine'],
+    ['National blockchain driven private cloud-based platform for Bangladesh: adopted in land administration', 'Decentralized Identity and Aid Distribution Platform for Rohingya Refugees'],
+    ['MediTech Bangla', 'PROBAHO - Decentralized VAT Automation Network for Ensuring Economic Growth'],
+    ['RETransparentNET, A Platform for Secure Transaction in Real Estate Industry', 'Arun'],
+    ['ReliChain : Trust and transparency in relief supply chain', 'Blockchain to Prevent Public Health Risks caused by Counterfeit Skin and Health Care Products'],
+    ['BONTON, A Transparent Relief Distribution Using Blockchain Technology', 'AprChain'],
+    ['CampusSafe, Helping Students to Get Back to Schools Safely', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT'],
+    ['xxxx', 'justbOT']
+];
+
+var img = [
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png'],
+    ['./images/2020-IBCOL-Finalists/uni/hk/cuhk246.png', './images/2020-IBCOL-Finalists/uni/hk/cuhk246.png']
+];
+
+var meritWinnersLink = [
+    ['', ''],
+    ['', ''],
+    ['', ''],
+    ['', ''],
+    ['', ''],
+    ['', '']
+];
+
+var allAwardTeams = document.getElementById('allAwardTeams');
+
+// for (let i = 0; i <= 18; i++) {
+//     if (i == 0) {
+//         allAwardTeams.innerHTML += (
+//             '<div class="row" id="awardRow' + i + '">' + '</div>'
+//         );
+//     } else {
+//         allAwardTeams.innerHTML += (
+//             '<div class="row mt-5" id="awardRow' + i + '">' + '</div>'
+//         );
+//     }
+//     var awardRowX = document.getElementById('awardRow' + i);
+//     for (let j = 0; j <=1; j++) {
+//         if(i <= 6){
+//             var targetCountry = "bd";
+//         } else if(i == 7) {
+//             var targetCountry = "ca";
+//         }
+//         if (j == 1) {
+//             awardRowX.innerHTML += (
+//                 '<div class="col-md-6 col-sm-12 centre mt-5 mt-md-0"  onclick="meritWinners('+ "'"+targetCountry + indexArr[i][j]+"'" +')">' +
+//                 '<div class="">' +
+//                 '<div class="" id="winnerInfo">' +
+//                 '<div class="row w-100 mx-0">' +
+//                 '<div class="col-auto col-sm-4 text-right-413-centre">' +
+//                 '<img class="winners2020Img2" src="' + img[i][j] + '">' +
+//                 '</div>' +
+//                 '<div class="col px-0 my-auto text-left-413-centre ">' +
+//                 '<div class="boldText textBlack fs-14 ">' +
+//                 teamNameAndID[i][j] +
+//                 '</div>' +
+//                 '<div class="fs-italicized fs-12 textBlack ">' +
+//                 pjName[i][j] +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>'
+//             );
+//         } else {
+//             awardRowX.innerHTML += (
+//                 '<div class="col-md-6 col-sm-12 centre" onclick="meritWinners('+ "'"+targetCountry + indexArr[i][j]+"'" +')">' +
+//                 '<div class="">' +
+//                 '<div class="" id="winnerInfo">' +
+//                 '<div class="row w-100 mx-0">' +
+//                 '<div class="col-auto col-sm-4 text-right-413-centre">' +
+//                 '<img class="winners2020Img2" src="' + img[i][j] + '">' +
+//                 '</div>' +
+//                 '<div class="col px-0 my-auto text-left-413-centre ">' +
+//                 '<div class="boldText textBlack fs-14 ">' +
+//                 teamNameAndID[i][j] +
+//                 '</div>' +
+//                 '<div class="fs-italicized fs-12 textBlack ">' +
+//                 pjName[i][j] +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>' +
+//                 '</div>'
+//             );
+//         }
+//     }
+// }
+
+function meritWinners(target) {
+
+    var bd = [
+        './2020EXPO/2020-IBCOL-Finalists/HK-12-representatives/BCrew/index.html',
+        '02'
+    ];
+
+    var ca = [
+        './2020EXPO/2020-IBCOL-Finalists/HK-12-representatives/Microcent/index.html',
+        '02'
+    ];
+
+    var countryCode = target.slice(0, 2);
+    var index = parseInt(target.slice(2, 4));
+
+
+    switch (checkCountries(countryCode)) {
+        case 'bd':
+            window.open(bd[index - 1]);
+            break;
+
+        case 'ca':
+            window.open(ca[index - 1]);
+            break;
+    }
+}
+
+function checkCountries(countryCode) {
+    var showArr = ['bd', 'ca', 'cn', 'es', 'hk', 'mn', 'nl', 'ph', 'pl', 'th', 'tr', 'tw', 'uk', 'us', 'vn'];
+    for (let i = 0; i <= showArr.length; i++) {
+        if (countryCode == showArr[i]) {
+            return showArr[i];
+        }
+    }
+}
